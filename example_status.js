d334ac5b1f08bfab44cd6358c62a2a97f205ef6a
Taylor Growden <taylor.growden@gmail.com>
2015-12-15 00:18:11 -0500

    frontend tweaks

---
+++ example_status.js
@@ -0,0 +1,5 @@
+var STATUS = "production"; //set STATUS to 'production', 'dev', or 'dev-local'
+
+module.exports = {
+	STATUS
+};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app      = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-global.STATUS = "production"; //set STATUS to 'production', 'dev', or 'dev-local'
+global.STATUS = require('./status');
 if (STATUS == "production"){
 	var port     = process.env.PORT || 8001;
 } else {
